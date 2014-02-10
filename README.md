5c876f8e149d1216cbaf9fdd78b550461af44f18
Gui Lin <guileen@gmail.com>
2014-02-10 18:10:30 +0800

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,56 @@ rts
 ===
 
 Redis time series data store
+
+Start rtsd service first
+
+    rtsd -p 6379 -h 127.0.0.1 -g '1s, 5m, 1h, 1d, 1M' --points 10000 --aggrgran='1y'
+
+## Init
+
+var rts = require('rts')(options);
+
+    var rts = require('rts')({
+            redis: redis,
+            gran: '5m, 1h, 1d, 1w',
+            points: 500,
+            prefix: ''
+        });
+
+Or, you can specify a namespace by prefix.
+
+    var foorts = require('rts')(redis, 'foo_');
+
+## Record
+
+record(key, \[num\], \[statistics\], \[aggregations\])
+
+statistics:
+avg
+max
+min
+
+aggragations
+hm aggragation by hour of day for each month
+hq    for each season
+hy    for each year
+dm aggragation by day of week.
+dq    for each season
+dy    for each year
+
+    ts.record('click')
+    ts.record('consume', 5, null, ['hm', 'dq'])
+    ts.record('delay', 100, ['avg','max','min'])
+
+the data will send to rtsd by redis pub/sub.
+
+## Query
+
+ts.get('click', '5m', fromDate, \[toDate\], callback)
+ts.count('delay', '5m', fromDate, \[toDate\], callback)
+ts.avg('delay', '5m', fromDate, \[toDate\], callback)
+ts.max('delay', '5m', fromDate, \[toDate\], callback)
+ts.min('delay', '5m', fromDate, \[toDate\], callback)
+
+ts.hours('consume', 'yyyy')
+ts.days('consume', 'yyyy')
---
+++ test/test.js
@@ -0,0 +1,42 @@
+var redis = require('redis');
+var client = redis.createClient();
+var rts = require('../lib/rts')({
+        redis: client
+      , gran: '5m, 1h, 1d, 1w'
+      , points: 500
+      , prefix: 'test'
+});
+
+var H = 60 * 60 * 1000;
+var D = 24 * H;
+var t = Date.now() - 1000 * H;
+var from = t;
+
+function record(timestamp, name, num, statistics, aggregations, callback) {
+    rts.record(name, num, statistics, aggregations, timestamp, callback);
+}
+
+function _log(message) {
+    return function log(err, results) {
+        console.log(message, err, results);
+    }
+}
+
+var timer = setInterval(function() {
+    t += Math.random() * H;
+    record(t, 'click');
+    record(t, 'delay', Math.random() * 100, ['min', 'max', 'avg']);
+    record(t, 'consume', Math.random() * 1000, ['avg', 'min', 'max', 'sum'], ['dy', 'hq']);
+}, 10);
+timer.unref();
+
+setTimeout(function() {
+        rts.sum('click', '5m', from, from + H,  _log('click 5m'));
+        rts.sum('click', '1d', from, from + 30 * D, _log('click 1d'));
+        rts.avg('delay', '1h', from, from + 24 * H, _log('delay 1h avg'));
+        rts.max('delay', '1w', from, from + 365 * D, _log('delay 1w max'));
+        rts.aggrmin('consume', 'dy', from, _log('consume dy'));
+        rts.aggravg('consume', 'hq', from, _log('consume hq'));
+        client.unref();
+}, 1000);
+
