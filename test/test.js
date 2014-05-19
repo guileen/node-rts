9fd42b382f17f55686a9026904fb83ce5af7fcd9
Gui Lin <guileen@gmail.com>
2014-05-19 17:46:54 +0800

    support record unique

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ var rts = require('../lib/rts')({
       , gran: '5m, 1h, 1d, 1w'
       , points: 500
       , prefix: 'test'
+      , interval: 1000
 });
 
 var H = 60 * 60 * 1000;
@@ -16,20 +17,30 @@ function record(timestamp, name, num, statistics, aggregations, callback) {
     rts.record(name, num, statistics, aggregations, timestamp, callback);
 }
 
+function recordUnique(timestamp, name, uniqueId, statistics, aggregations, callback) {
+    rts.recordUnique(name, uniqueId, statistics, aggregations, timestamp, callback);
+}
+
 function _log(message) {
     return function log(err, results) {
         console.log(message, err, results);
     }
 }
 
-var timer = setInterval(function() {
-    t += Math.random() * H;
-    record(t, 'click');
-    record(t, 'delay', Math.random() * 101, ['min', 'max', 'avg']);
-    record(t, 'consume', Math.random() * 1000, ['avg', 'min', 'max', 'sum'], ['dy', 'hq']);
-}, 10);
+// var timer = setInterval(function() {
+    for(var i=0;i<1000;i++) {
+      t += Math.random() * 60 * 1000;
+      recordUnique(t, 'access', 'u' + Math.floor(Math.random() * 100));
+      record(t, 'click');
+      record(t, 'delay', Math.random() * 101, ['min', 'max', 'avg']);
+      record(t, 'consume', Math.random() * 1000, ['avg', 'min', 'max', 'sum'], ['dy', 'hq']);
+    }
+// }, 10);
 
 setTimeout(function() {
+        rts.unique('access', '5m', from, from + 5* H, _log('access 5m'));
+        rts.unique('access', '1h', from, from + 24* H, _log('access 1h'));
+        rts.unique('access', '1d', from, from + 5 * D, _log('access 1d'));
         rts.sum('click', '5m', from, from + H,  _log('click 5m'));
         // even not store, also can get, but zero results;
         rts.sum('click', '8m', from, from + H,  _log('click 8m'));
@@ -38,7 +49,7 @@ setTimeout(function() {
         rts.max('delay', '1w', from, from + 365 * D, _log('delay 1w max'));
         rts.aggrmin('consume', 'dy', from, _log('consume dy'));
         rts.aggravg('consume', 'hq', from, _log('consume hq'));
-        clearInterval(timer);
-        client.unref();
-}, 1000);
+        // clearInterval(timer);
+        // client.unref();
+}, 2000);
 
